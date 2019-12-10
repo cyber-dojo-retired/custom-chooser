a25acd6c953cee440062ca8ce834cbd46cf4e6e5
JonJagger <jon@jaggersoft.com>
2019-12-10 21:46:20 +0000

    Simplify js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,13 +2,8 @@
 'use strict';
 var cyberDojo = (function(cd, $) {
 
-  cd.switchEntry = (previous, current) => {
-    if (previous !== undefined) {
-      $(previous).removeClass('selected');
-    }
-    $(current).addClass('selected');
-  };
-
+  //...
+  
   return cd;
 
 })(cyberDojo || {}, jQuery);
