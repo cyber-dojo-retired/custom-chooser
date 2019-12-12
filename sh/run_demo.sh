22fead3f8ba4cf557f88e159949d1cf24aa1635e
JonJagger <jon@jaggersoft.com>
2019-12-12 10:28:31 +0000

    Refactoring; use simpler style when declaring variables in CircleCI config.yml

--- sh/run_demo.sh
+++ sh/run_demo.sh
@@ -8,10 +8,13 @@ export $(docker run --rm cyberdojo/versioner:latest sh -c 'cat /app/.env')
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
 
-if [ ! -z "${DOCKER_MACHINE_NAME}" ]; then
-  declare ip=$(docker-machine ip "${DOCKER_MACHINE_NAME}")
-else
-  declare ip=localhost
-fi
+ip_address()
+{
+  if [ -n "${DOCKER_MACHINE_NAME}" ]; then
+    docker-machine ip ${DOCKER_MACHINE_NAME}
+  else
+    echo localhost
+  fi
+}
 
-open "http://${ip}:${PORT}/custom/show?from=individual"
+open "http://$(ip_address):${PORT}/custom/show?from=individual"
