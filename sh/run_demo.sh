1e143cfaccfa5ae0b3e91f8384edb415ac0583f3
JonJagger <jon@jaggersoft.com>
2019-12-15 10:15:58 +0000

    Added http curl examples and deprecated routes examples to demo

--- sh/run_demo.sh
+++ sh/run_demo.sh
@@ -28,17 +28,48 @@ json_curl()
     --silent \
     -X POST \
     "http://$(ip_address):${PORT}/${URL}" \
-      | jq .id
+      | jq -r .id
+}
+
+http_curl()
+{
+  URL=${1}
+  LOG=/tmp/custom.log
+  DISPLAY_NAME='Java Countdown, Round 1'
+  X_DISPLAY_NAME='Java%20Countdown%2C%20Round%201'
+  curl  \
+    --header 'Accept: text/html' \
+    --silent \
+    --verbose \
+    -X POST \
+    "http://$(ip_address):${PORT}/${URL}?display_name=${X_DISPLAY_NAME}" \
+     > ${LOG} 2>&1
+  grep --quiet 302 ${LOG}          # HTTP/1.1 302 Moved Temporarily
+  LOCATION=$(grep Location ${LOG}) # Location: http://192.168.99.100/kata/edit/mzCS1h
+  echo -n ${LOCATION:(-6)}
 }
 
 echo
-echo Json
+echo HTTP
+printf "custom/create_kata  => $(http_curl custom/create_kata)\n"
+printf "custom/create_group => $(http_curl custom/create_group)\n"
+
+echo
+echo JSON
 printf "custom/create_kata  => $(json_curl custom/create_kata)\n"
 printf "custom/create_group => $(json_curl custom/create_group)\n"
 
-echo 'Deprecated Json (nginx redirect)'
-printf ".../save_individual_json => $(json_curl setup_custom_start_point/save_individual_json)\n"
-printf ".../save_group_json      => $(json_curl setup_custom_start_point/save_group_json)\n"
+echo
+OLD_CONTROLLER=setup_custom_start_point
+echo "Deprecated (nginx redirect) ${OLD_CONTROLLER}/..."
+echo HTTP
+printf ".../save_individual_json => $(http_curl ${OLD_CONTROLLER}/save_individual_json)\n"
+printf ".../save_group_json      => $(http_curl ${OLD_CONTROLLER}/save_group_json)\n"
+
+echo
+echo JSON
+printf ".../save_individual_json => $(json_curl ${OLD_CONTROLLER}/save_individual_json)\n"
+printf ".../save_group_json      => $(json_curl ${OLD_CONTROLLER}/save_group_json)\n"
 echo
 
-#open "http://$(ip_address):${PORT}/custom/index?for=kata"
+open "http://$(ip_address):${PORT}/custom/index?for=kata"
