9fffb035cb3536fd0011f2395ed88cfefd70d108
Jon Jagger <jon@jaggersoft.com>
2020-02-24 08:13:57 +0000

    Give each http-service its own http adapter

--- src/server/code/externals.rb
+++ src/server/code/externals.rb
@@ -6,15 +6,17 @@ require_relative 'external_http'
 class Externals
 
   def creator
-    @creator ||= ExternalCreator.new(http)
+    @creator ||= ExternalCreator.new(creator_http)
+  end
+  def creator_http
+    @creator_http ||= ExternalHttp.new
   end
 
   def custom_start_points
-    @custom_start_points ||= ExternalCustomStartPoints.new(http)
+    @custom_start_points ||= ExternalCustomStartPoints.new(custom_start_points_http)
   end
-
-  def http
-    @http ||= ExternalHttp.new
+  def custom_start_points_http
+    @custom_start_points_http ||= ExternalHttp.new
   end
 
 end
