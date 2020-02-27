15d465e24e59565b07e67561edde34a6474a0a69
Jon Jagger <jon@jaggersoft.com>
2020-02-27 12:56:58 +0000

    Add GET-302 example to api-demo.sh

--- sh/api_demo.sh
+++ sh/api_demo.sh
@@ -29,8 +29,11 @@ demo()
   curl_json_body_200 GET  ready
   curl_json_body_200 GET  sha
   echo
-  curl_json_body_200 POST create_kata  "$(json_display_name)"
+  curl_params_302    GET  create_group "$(params_display_name)"
+  curl_params_302    GET  create_kata  "$(params_display_name)"
+  echo
   curl_json_body_200 POST create_group "$(json_display_name)"
+  curl_json_body_200 POST create_kata  "$(json_display_name)"
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -38,7 +41,7 @@ curl_json_body_200()
 {
   local -r log=/tmp/creator.log
   local -r type="${1}"   # eg GET|POST
-  local -r route="${2}"  # eg sha
+  local -r route="${2}"  # eg create_group
   local -r json="${3:-}" # eg '{"display_name":"Java Countdown, Round 1"}'
   curl  \
     --data "${json}" \
@@ -57,8 +60,30 @@ curl_json_body_200()
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - -
-port() { echo "${CYBER_DOJO_CUSTOM_CHOOSER_PORT}"; }
+curl_params_302()
+{
+  local -r log=/tmp/creator.log
+  local -r type="${1}"     # eg GET|POST
+  local -r route="${2}"    # eg create_kata
+  local -r params="${3:-}" # eg "display_name=Java Countdown, Round 1"
+  curl  \
+    --data-urlencode "${params}" \
+    --fail \
+    --request "${type}" \
+    --silent \
+    --verbose \
+      "http://${IP_ADDRESS}:$(port)/${route}" \
+      > "${log}" 2>&1
+
+    grep --quiet 302 "${log}"                 # eg HTTP/1.1 302 Moved Temporarily
+    local -r result=$(grep Location "${log}") # Location: http://192.168.99.100:4536/kata/edit/5B65RC
+    echo "$(tab)${type} ${route} => 302 ${result}"
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - -
+port() { echo -n "${CYBER_DOJO_CUSTOM_CHOOSER_PORT}"; }
 json_display_name() { echo -n '{"display_name":"Java Countdown, Round 1"}'; }
+params_display_name() { echo -n "display_name=Java Countdown, Round 1"; }
 tab() { printf '\t'; }
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - -
