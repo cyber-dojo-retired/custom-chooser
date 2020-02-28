a8e6f536a351b33d3afc0a2d9f92e2580242633a
Jon Jagger <jon@jaggersoft.com>
2020-02-28 14:52:21 +0000

    Fix image-name

--- sh/versioner_env_vars.sh
+++ sh/versioner_env_vars.sh
@@ -4,7 +4,7 @@ versioner_env_vars()
 {
   docker run --rm cyberdojo/versioner:latest
   # TODO: these will move into cyberdojo/versioner
-  echo CYBER_DOJO_CUSTOM_CHOOSER_IMAGE=cyberdojo/custom_chooser
+  echo CYBER_DOJO_CUSTOM_CHOOSER_IMAGE=cyberdojo/custom-chooser
   echo CYBER_DOJO_CUSTOM_CHOOSER_PORT=4536
   echo CYBER_DOJO_CUSTOM_CHOOSER_CLIENT_PORT=4537
 }
