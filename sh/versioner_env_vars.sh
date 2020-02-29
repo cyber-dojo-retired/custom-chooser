fe60315fa5f715fdea00acb1d7e0b33bcbdf5dd2
Jon Jagger <jon@jaggersoft.com>
2020-02-29 12:59:58 +0000

    Refactoring; delete dead code

--- sh/versioner_env_vars.sh
+++ sh/versioner_env_vars.sh
@@ -3,8 +3,5 @@
 versioner_env_vars()
 {
   docker run --rm cyberdojo/versioner:latest
-  # TODO: these will move into cyberdojo/versioner
-  echo CYBER_DOJO_CUSTOM_CHOOSER_IMAGE=cyberdojo/custom-chooser
-  echo CYBER_DOJO_CUSTOM_CHOOSER_PORT=4536
   echo CYBER_DOJO_CUSTOM_CHOOSER_CLIENT_PORT=4537
 }
