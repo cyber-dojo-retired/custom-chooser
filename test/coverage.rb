9425b3a09f1fd7afe1aee2e072a7efcc0d1c61a1
Jon Jagger <jon@jaggersoft.com>
2020-03-01 09:35:02 +0000

    Refactoring; extract identifier

--- test/coverage.rb
+++ test/coverage.rb
@@ -1,14 +1,13 @@
 require 'simplecov'
 
+code_name = ENV['COVERAGE_CODE_GROUP_NAME']
+test_name = ENV['COVERAGE_TEST_GROUP_NAME']
+
 SimpleCov.start do
   #enable_coverage :branch
   filters.clear
   coverage_dir(ENV['COVERAGE_ROOT'])
   #add_group('debug') { |src| puts src.filename; false }
-  add_group(ENV['COVERAGE_CODE_GROUP_NAME']) { |src|
-    src.filename =~ %r"^/app/"
-  }
-  add_group(ENV['COVERAGE_TEST_GROUP_NAME']) { |src|
-    src.filename =~ %r"^/test/.*_test\.rb$"
-  }
+  add_group(code_name) { |src| src.filename =~ %r"^/app/" }
+  add_group(test_name) { |src| src.filename =~ %r"^/test/.*_test\.rb$" }
 end
