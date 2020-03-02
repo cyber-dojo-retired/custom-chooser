494b8fbb9d6a45cd45a359cf3f8e976d0e5d8d6d
Jon Jagger <jon@jaggersoft.com>
2020-03-02 07:55:12 +0000

    Refactoring; in coverage tab rename group->tab to avoid group/kata confusion

--- test/coverage.rb
+++ test/coverage.rb
@@ -1,13 +1,13 @@
 require 'simplecov'
 
-code_name = ENV['COVERAGE_CODE_GROUP_NAME']
-test_name = ENV['COVERAGE_TEST_GROUP_NAME']
+code_tab = ENV['COVERAGE_CODE_TAB_NAME']
+test_tab = ENV['COVERAGE_TEST_TAB_NAME']
 
 SimpleCov.start do
   #enable_coverage :branch
   filters.clear
   coverage_dir(ENV['COVERAGE_ROOT'])
   #add_group('debug') { |src| puts src.filename; false }
-  add_group(code_name) { |src| src.filename =~ %r"^/app/" }
-  add_group(test_name) { |src| src.filename =~ %r"^/test/.*_test\.rb$" }
+  add_group(code_tab) { |src| src.filename =~ %r"^/app/" }
+  add_group(test_tab) { |src| src.filename =~ %r"^/test/.*_test\.rb$" }
 end
