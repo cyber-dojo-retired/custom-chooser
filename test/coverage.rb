fc70948adb58ccd0eacdaee9b4cffcd817da9a16
Jon Jagger <jon@jaggersoft.com>
2020-03-02 07:59:39 +0000

    Refactoring; move statements to more local scope

--- test/coverage.rb
+++ test/coverage.rb
@@ -1,13 +1,12 @@
 require 'simplecov'
 
-code_tab = ENV['COVERAGE_CODE_TAB_NAME']
-test_tab = ENV['COVERAGE_TEST_TAB_NAME']
-
 SimpleCov.start do
   #enable_coverage :branch
   filters.clear
   coverage_dir(ENV['COVERAGE_ROOT'])
   #add_group('debug') { |src| puts src.filename; false }
+  code_tab = ENV['COVERAGE_CODE_TAB_NAME']
+  test_tab = ENV['COVERAGE_TEST_TAB_NAME']
   add_group(code_tab) { |src| src.filename =~ %r"^/app/" }
   add_group(test_tab) { |src| src.filename =~ %r"^/test/.*_test\.rb$" }
 end
