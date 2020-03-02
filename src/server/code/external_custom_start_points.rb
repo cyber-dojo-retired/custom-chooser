bac687e5d057a46aba08768f0c5e34fd78c7612c
Jon Jagger <jon@jaggersoft.com>
2020-03-02 04:42:15 +0000

    Refactoring; delete dead code

--- src/server/code/external_custom_start_points.rb
+++ src/server/code/external_custom_start_points.rb
@@ -7,28 +7,12 @@ class ExternalCustomStartPoints
     @http = HttpJsonHash::service(self.class.name, http, 'custom-start-points', 4526)
   end
 
-  def alive?
-    @http.get(__method__, {})
-  end
-
   def ready?
-    @http.get(__method__, {})
-  end
-
-  def sha
     @http.get(__method__, {})
   end
 
   def display_names
     @http.get(:names, {})
-  end
-
-  def manifests
-    @http.get(__method__, {})
-  end
-
-  def manifest(display_name)
-    @http.get(__method__, { name:display_name })
   end
 
 end
